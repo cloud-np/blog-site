dc266155398ad4552d6a1355c5b5ba8bc8816ad8
cloud-np <cloud-np@outlook.com>
2022-10-08 14:01:13 +0300

    Initial commit - Changed major css styling and adding custom components like TypeWriter

---
+++ src/data/socials.ts
@@ -0,0 +1,12 @@
+export const socials = [
+  {
+    name: 'Twitter',
+    url: 'https://www.linkedin.com/in/cloud-np/',
+    icon: 'mdi:linkedin'
+  },
+  {
+    name: 'GitHub',
+    url: 'https://github.com/cloud-np',
+    icon: 'mdi:github'
+  }
+]
