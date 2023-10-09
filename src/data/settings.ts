37b7d5794529d5d13c403125d2c27293aaf7bf03
cloud-np <cloud-np@outlook.com>
2023-10-09 22:46:00 +0200

    feat: update astro and fix typewriter bug

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,5 +1,5 @@
 export const settings = {
-  site: 'https://cloud-np.com',
+  url: 'https://cloud-np.com',
   name: 'cloud-np',
   title: 'cloud-np - A blog about computer science.',
   description: 'A blog about computer science, programming, software engineering, and technology. No ads, no tracking, no bullshit.',
