cfd099ab120c4f39775e9bfb3e0a79b2912aabfa
cloud-np <cloud-np@outlook.com>
2023-12-04 00:58:31 +0200

    feat: build half of the first post and add custom comps in the mdx file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,23 @@ import { defineConfig } from 'astro/config';
 import { settings } from './src/data/settings';
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 // https://astro.build/config
 export default defineConfig({
   site: settings.url,
-  integrations: [sitemap(), mdx()],
+  integrations: [
+    sitemap(),
+    mdx()
+  ],
   vite: {
     ssr: {
       external: ["svgo"]
     }
+  },
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
   }
 });
