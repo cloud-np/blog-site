f2cb141a97f594a01aee22a891d31de53b44c189
cloud-np <cloud-np@outlook.com>
2024-08-24 17:54:54 +0300

    fix: because of switch to react qwik compontents are failing typescript check

--- src/components/qwik/ThemeToggleButton/ThemeToggleButton.tsx
+++ src/components/qwik/ThemeToggleButton/ThemeToggleButton.tsx
@@ -1,28 +1,28 @@
-import { component$, useSignal, useStylesScoped$, type Signal } from "@builder.io/qwik";
-import styles from './ThemeToggleButton.css?inline';
-import { ThemeType, ThemeUtil } from "./theme";
-import { lightThemeIcon, darkThemeIcon } from './icons';
+// import { component$, useSignal, useStylesScoped$, type Signal } from "@builder.io/qwik";
+// import styles from './ThemeToggleButton.css?inline';
+// import { ThemeType, ThemeUtil } from "./theme";
+// import { lightThemeIcon, darkThemeIcon } from './icons';
 
-const THEME_KEY = "theme";
+// const THEME_KEY = "theme";
 
-const setTheme = (theme: Signal<ThemeType>) => {
-    // TODO: Could this be wrong since we are not using the libraries way to access the el?
-    // https://qwik.dev/docs/components/overview/#getting-hold-of-dom-element
-    document.firstElementChild?.setAttribute('color-scheme', theme.value);
-    theme.value = ThemeUtil.getOppositeTheme(theme.value);
-    localStorage.setItem(THEME_KEY, theme.value);
-}
+// const setTheme = (theme: Signal<ThemeType>) => {
+//     // TODO: Could this be wrong since we are not using the libraries way to access the el?
+//     // https://qwik.dev/docs/components/overview/#getting-hold-of-dom-element
+//     document.firstElementChild?.setAttribute('color-scheme', theme.value);
+//     theme.value = ThemeUtil.getOppositeTheme(theme.value);
+//     localStorage.setItem(THEME_KEY, theme.value);
+// }
 
-export const ThemeToggleButton = component$(() => {
-    useStylesScoped$(styles);
-    const theme = useSignal(ThemeType.dark);
+// export const ThemeToggleButton = component$(() => {
+//     useStylesScoped$(styles);
+//     const theme = useSignal(ThemeType.dark);
 
-    return <button
-        onClick$={() => setTheme(theme)}
-        title={`Enable ${ThemeUtil.getOppositeTheme(theme.value)} Theme`}
-    >
-        {theme.value === ThemeType.dark
-          ? lightThemeIcon
-          : darkThemeIcon}
-    </button>
-});
+//     return <button
+//         click$={() => setTheme(theme)}
+//         title={`Enable ${ThemeUtil.getOppositeTheme(theme.value)} Theme`}
+//     >
+//         {theme.value === ThemeType.dark
+//           ? lightThemeIcon
+//           : darkThemeIcon}
+//     </button>
+// });
